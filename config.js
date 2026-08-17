ebad788ed294607e59df7f56827f7144aa186b25
ticketwaves <ticketwavesofficial@gmail.com>
2026-08-17 13:04:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.TICKETWAVES_API_URL='https://ticketwaves-backend-k1hs.onrender.com';
-window.TICKETWAVES_FRONTEND_URL='https://yasiyrshehu-byte.github.io/TicketWAVES-frontend';
+window.TICKETWAVES_FRONTEND_URL='https://ticketwaves.github.io/TicketWAVES-frontend';
