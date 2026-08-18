639a241ecb6fee944c528544abc47d544f75a493
ticketwaves <ticketwavesofficial@gmail.com>
2026-08-18 18:00:13 +0100

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Set this to your backend URL WITHOUT /api.
-// Example: https://ticketwaves-backend-k1hs.onrender.com
-window.TICKETWAVES_API_URL = 'https://ticketwaves-backend-k1hs.onrender.com';
-window.TICKETWAVES_FRONTEND_URL = 'https://ticketwaves.github.io/TicketWAVES-frontend';
