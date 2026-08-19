a68e989e0f9a52fdf9a6a9a93aadad30f1407d30
ticketwaves <ticketwavesofficial@gmail.com>
2026-08-19 11:41:40 +0100

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// TicketWAVES frontend configuration
-window.TICKETWAVES_API_URL='https://ticketwaves-backend-k1hs.onrender.com';
-window.TICKETWAVES_FRONTEND_URL='https://ticketwaves.github.io/TicketWAVES-frontend';
